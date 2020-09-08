3d021c082f0d20df7280f028a814147a29113dd6
Lindany <mabasolindaniricardo@gmail.com>
2020-09-08 09:37:00 +0200

    add questions and spam (url) check

--- customer-comment-analyzer/src/com/ikhokha/techcheck/CommentAnalyzer.java
+++ customer-comment-analyzer/src/com/ikhokha/techcheck/CommentAnalyzer.java
@@ -40,6 +40,18 @@ public class CommentAnalyzer {
 					incOccurrence(resultsMap, "SHAKER_MENTIONS");
 				
 				}
+				if (line.contains("?")) {
+
+					incOccurrence(resultsMap, "QUESTION_MENTIONS");
+				
+				}
+				
+				//if(new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(line)
+				if (line.contains("http://") || line.contains("https://") ) {
+
+					incOccurrence(resultsMap, "SPAM_MENTIONS");
+				
+				}
 			}
 			
 		} catch (FileNotFoundException e) {
